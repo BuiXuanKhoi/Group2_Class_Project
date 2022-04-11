13d972a862b2c75611f7a909aa79e601a7add8f9
Bui Xuan Khoi <101469417+BuiXuanKhoi@users.noreply.github.com>
2022-04-11 14:22:04 +0700

    Delete Week5-Group2 directory

--- Week5-Group2/Week4-Group2/app/src/main/java/com/example/week2_group2/Data/Info.kt
+++
@@ -1,3 +0,0 @@
-package com.example.week2_group2.Data
-
-data class Info(val id: Int, val name: String, val address: String, val link: String)
--- Week5-Group2/Week4-Group2/app/src/main/java/com/example/week2_group2/Data/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.week2_group2.Data
-
-data class User(val id: Int?,val name:String?, val email:String?,val pass:String?)
